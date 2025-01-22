789eeb42ee329678bd4b37f2775fe1691f94c9e5
Ritesh <riteshroutray2005@gmail.com>
2025-01-22 21:07:06 +0530

    controllers

--- backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express, {Router} from 'express'
-import { question } from '../controllers/index.js';
-
-const router = Router();
-
-router.route('/',).post(question)
-
-
-export default router;
